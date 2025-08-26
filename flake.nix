e3d84fddcca2ce03f2e8b7bdc05ae8f42045d5c1
Andrew McMiddlin <andrew.mcmiddlin@paidright.io>
2025-08-26 13:10:40 +1000

    Patch python so dagster has what it needs.

--- flake.nix
+++ flake.nix
@@ -10,12 +10,30 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        # taken from https://wiki.nixos.org/wiki/Python#Prefix_library_paths_using_wrapProgram
+        pythonldlibpath = pkgs.lib.makeLibraryPath (with pkgs; [
+            zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
+          ]);
+
+        # Darwin requires a different library path prefix
+        wrapPrefix = if (pkgs.stdenv.isDarwin) then "DYLD_LIBRARY_PATH" else "LD_LIBRARY_PATH";
+        patchedPython = (pkgs.symlinkJoin {
+          name = "python";
+          paths = [ pkgs.python312 ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram "$out/bin/python3.12" --prefix ${wrapPrefix} : "${pythonldlibpath}"
+          '';
+        });
       in
       {
+        dupkgs = pkgs;
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs;
             [
-              python3
+              patchedPython
               uv
             ];
         };
